7a64c41fef0cdf4d3f8049a1a0e70b1bb1462e18
hirata-kiyono <y.hirata@kiyono-co.jp>
2022-08-13 05:37:43 +0900

    update to send message

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/hirata-kiyono/github_cron_test
+
+go 1.18
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/slack-go/slack v0.11.2 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
 )
 
 func main() {
+	TOKEN := os.Getenv("TOKEN")
+	CHANNEL := os.Getenv("CHANNEL")
+
+	c := slack.New(TOKEN)
+	_, _, err := c.PostMessage(CHANNEL, slack.MsgOptionText("Hello World", true))
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Hello World")
 }
