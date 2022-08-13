7bc3a0d90f1a9a3bb3606f870e13445e58f0d373
hirata-kiyono <y.hirata@kiyono-co.jp>
2022-08-13 11:21:16 +0900

    update update contents

--- main.go
+++ main.go
@@ -12,9 +12,26 @@ func main() {
 	CHANNEL := os.Getenv("CHANNEL")
 
 	c := slack.New(TOKEN)
-	_, _, err := c.PostMessage(CHANNEL, slack.MsgOptionText("Hello World", true))
+
+	params := &c.GetConversationHistoryParameters{
+		ChannelID:          CHANNEL,
+		Cursor:             "",
+		Inclusive:          false,
+		Latest:             "",
+		Limit:              5,
+		Oldest:             "",
+		IncludeAllMetadata: false,
+	}
+
+	res, err := c.GetConversationHistory(params)
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Printf("%v", res)
+	// _, _, err = c.PostMessage(CHANNEL, slack.MsgOptionText("Hello World", true))
+	// if err != nil {
+	// 	panic(err)
+	// }
 	fmt.Println("Hello World")
 }
